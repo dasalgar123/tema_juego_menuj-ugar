12c91a7c67897be808e6cd228336a9f678ed874e
dasalgar123 <106126696+dasalgar123@users.noreply.github.com>
2024-10-08 20:16:19 -0500

    Add files via upload

---
+++ juegar.js
@@ -0,0 +1,5 @@
+document.getElementById('btnJugarSolo').addEventListener('click', () => {
+    const numJugadas = document.getElementById('numJugadas').value;
+    alert(`Vas a jugar ${numJugadas} veces en el modo solo.`);
+    // Implementar la lógica del juego aquí
+});
